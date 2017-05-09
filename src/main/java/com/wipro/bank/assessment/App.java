0749ba41ffbe344a0cb2e671da67dabbede4f635
Dheenadhayalan <Dillibabu.Dheenadhayalan@standardbank.co.za>
2017-05-09 09:55:48 +0200

    implemented getAccountsDetailsByID method

--- src/main/java/com/wipro/bank/assessment/App.java
+++ src/main/java/com/wipro/bank/assessment/App.java
@@ -31,7 +31,7 @@ public class App
 		
 	}
 	
-	public List<Account> getAccountDetailsByBalance(double amount)
+	/*public List<Account> getAccountDetailsByBalance(double amount)
 	{
 		
 	
@@ -50,7 +50,7 @@ public class App
 		return emp2;
 		
 		
-	}
+	}*/
 	
     public static void main( String[] args )
     {
@@ -64,9 +64,9 @@ public class App
     	accounts.add(new Account(7,"acc7",701.11));
     	accounts.add(new Account(7,"acc8",801.99));
     	
-    	App temp = new App();
+    	/*App temp = new App();
     	temp.getAccountDetailsByID(1);
-    	temp.getAccountDetailsByBalance(501.77);
+    	temp.getAccountDetailsByBalance(501.77);*/
     	
        
     }
